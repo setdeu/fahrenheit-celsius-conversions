da3994ef9268438369cae6f7e46d1cd5ae5915b8
oliverds <oliverds@users.noreply.github.com>
2022-09-13 21:26:18 +0000

    Fix styling

--- src/Temperature.php
+++ src/Temperature.php
@@ -6,14 +6,14 @@ class Temperature
 {
     protected float $celsius;
 
-	public function __construct(float $celsius)
-	{
-        $this->celsius = $celsius;
-	}
-
-	public function toFahrenheit(): float
+    public function __construct(float $celsius)
     {
-        return ($this->celsius * (9/5)) + 32;
+        $this->celsius = $celsius;
+    }
+
+    public function toFahrenheit(): float
+    {
+        return ($this->celsius * (9 / 5)) + 32;
     }
 
     public static function celsius(float $celsius): self

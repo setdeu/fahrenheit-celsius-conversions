be82ea932178f07e8b62478bce710930f1dc464b
Oliver Servín <oliverds@users.noreply.github.com>
2022-09-13 21:10:53 -0500

    Revert "Push a failing test"
    This reverts commit 531d96931767f0617e1f3d540c79e9f0290ffd71.

--- tests/TemperatureTest.php
+++ tests/TemperatureTest.php
@@ -3,7 +3,7 @@
 use Setdeu\FahrenheitCelsiusConversions\Temperature;
 
 test('can convert celsius to fahrenheit', function () {
-    $fahrenheit = Temperature::celsius(200)->toFahrenheit();
+    $fahrenheit = Temperature::celsius(100)->toFahrenheit();
 
     expect($fahrenheit)->toEqual(212);
 });
